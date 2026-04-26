23b2890bd4be22ee83cdaac9d12087b9abb1a832
Prasad M <prasad@promaxevents.in>
2026-04-26 17:32:14 +0530

    Country Card

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://xcountries-backend.labs.crio.do/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data))
+      .catch((error) => console.error("Error fetching data: ", error));
+  }, []);
+
+  return (
+    <div className="grid-container">
+      {countries.map((country) => (
+        <div className="flag-card" key={country.abbr}>
+          <img
+            src={country.flag}
+            alt={`${country.name} flag`}
+          />
+          <p>{country.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
